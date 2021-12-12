a3bc881bc133488ce8640c13303e5d1c10d40f05
AlejandroCc1105 <bacobosc@unl.edu.ec>
2021-12-12 11:29:01 -0500

    tokens
    Modificación de Tokens

---
+++ ProyectoFinal/src/models/TokenV1.java
@@ -0,0 +1,22 @@
+package prueba;
+
+public class TokenV1 {
+    private String valor;
+    private Tipos tipo;
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Tipos getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipos tipo) {
+        this.tipo = tipo;
+    }
+}
